041a466df6ed8af093e682863d32439ce676d24f
KMJ1128 <kim23399551@gmail.com>
2026-05-04 15:35:52 +0900

    Initial commit

---
+++ src/main/java/com/pocket_plant/pocket_plant/PocketPlantApplication.java
@@ -0,0 +1,13 @@
+package com.pocket_plant.pocket_plant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PocketPlantApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PocketPlantApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pocket_plant/pocket_plant/PocketPlantApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pocket_plant.pocket_plant;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PocketPlantApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
